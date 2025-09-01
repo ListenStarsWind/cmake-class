743b8b90314066da6c04396cf5ae88b5a921c387
听风若依 <wind2106631469@gmail.com>
2025-09-01 21:19:25 +0800

    cmake指令-文本-上

---
+++ test_include/sub/sub.cmake
@@ -0,0 +1,10 @@
+message(STATUS "from sub/sub.cmake")
+
+# 打印源代码目录
+message(STATUS "CMAKE_CURRENT_SOURCE_DIR: ${CMAKE_CURRENT_SOURCE_DIR}")
+
+# 打印当前执行cmake脚本的完整名称
+message(STATUS "CMAKE_CURRENT_LIST_FILE: ${CMAKE_CURRENT_LIST_FILE}")
+
+# 打印当前执行cmake脚本的完整目录
+message(STATUS "CMAKE_CURRENT_LIST_DIR: ${CMAKE_CURRENT_LIST_DIR}")
